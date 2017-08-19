d408280b32542a3ea4a4e3b2293105603332a0e5
Agrim Prasad <AgrimPrasad@users.noreply.github.com>
2017-08-19 22:13:35 +0800

    RBQ: Use topic exchange with key pattern

--- log_consumer/main.go
+++ log_consumer/main.go
@@ -39,16 +39,28 @@ func Consume() {
 	)
 	failOnError(err, fmt.Sprintf("Failed to declare queue %s", q.Name))
 
-	exchName := "logs"
+	q2, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, fmt.Sprintf("Failed to declare queue %s", q2.Name))
+
+	exchName := "logs_topic"
+	ch1RoutingKey := "*.critical"
+	ch2RoutingKey := "kernel.*"
 
 	err = ch.QueueBind(
 		q.Name,
-		"",
+		ch1RoutingKey, // routing key
 		exchName,
 		false,
 		nil,
 	)
-	failOnError(err, fmt.Sprintf("Failed to bind queue %s to exchange %s", q.Name, exchName))
+	failOnError(err, fmt.Sprintf("Failed to bind queue %s with routing key %s", q.Name, ch1RoutingKey))
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -61,11 +73,42 @@ func Consume() {
 	)
 	failOnError(err, fmt.Sprintf("Failed to register consumer %s", q.Name))
 
+	err = ch.QueueBind(
+		q2.Name,
+		ch2RoutingKey, // routing key
+		exchName,
+		false,
+		nil,
+	)
+	failOnError(err, fmt.Sprintf("Failed to bind queue %s with routing key %s", q2.Name, ch2RoutingKey))
+
+	msgs2, err := ch.Consume(
+		q2.Name, // queue
+		"",      // consumer
+		false,   // auto-ack
+		false,   // exclusive
+		false,   // no-local
+		false,   // no-wait
+		nil,     // args
+	)
+	failOnError(err, fmt.Sprintf("Failed to register consumer %s", q2.Name))
+
 	forever := make(chan bool)
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message in queue %s: %s", q.Name, d.Body)
+			log.Printf("Received a message in queue with routing key %s: %s", ch1RoutingKey, d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done work")
+			d.Ack(false)
+		}
+	}()
+
+	go func() {
+		for d := range msgs2 {
+			log.Printf("Received a message in queue with routing key %s: %s", ch2RoutingKey, d.Body)
 			dotCount := bytes.Count(d.Body, []byte("."))
 			t := time.Duration(dotCount)
 			time.Sleep(t * time.Second)
--- log_publisher/main.go
+++ log_publisher/main.go
@@ -30,10 +30,10 @@ func Publish() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	exchName := "logs"
+	exchName := "logs_topic"
 	err = ch.ExchangeDeclare(
 		exchName, // name
-		"fanout", // type
+		"topic",  // type
 		true,     // durable
 		false,    // auto-deleted
 		false,    // internal
@@ -42,21 +42,50 @@ func Publish() {
 	)
 	failOnError(err, fmt.Sprintf("Failed to declare exchange %s", exchName))
 
-	input := make(chan string)
+	input := make(chan string, 3)
+
+	ch1RoutingKey := "app.critical"
+	ch2RoutingKey := "app.info"
+	ch3RoutingKey := "kernel.info"
 
 	go func() {
 		for {
 			err = ch.Publish(
-				exchName, // exchange
-				"",       // routing key
-				false,    // mandatory
-				false,    // immediate
+				exchName,      // exchange
+				ch1RoutingKey, // routing key
+				false,         // mandatory
+				false,         // immediate
 				amqp.Publishing{
 					ContentType: "text/plain",
 					Body:        []byte(<-input),
 				},
 			)
-			failOnError(err, fmt.Sprintf("Failed to publish a log to exchange %s", exchName))
+			failOnError(err, fmt.Sprintf("Failed to publish a log with routing key %s", ch1RoutingKey))
+
+			err = ch.Publish(
+				exchName,      // exchange
+				ch2RoutingKey, // routing key
+				false,         // mandatory
+				false,         // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(<-input),
+				},
+			)
+			failOnError(err, fmt.Sprintf("Failed to publish a log with routing key %s", ch2RoutingKey))
+
+			err = ch.Publish(
+				exchName,      // exchange
+				ch3RoutingKey, // routing key
+				false,         // mandatory
+				false,         // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(<-input),
+				},
+			)
+			failOnError(err, fmt.Sprintf("Failed to publish a log with routing key %s", ch3RoutingKey))
+
 		}
 	}()
 
